fb51b4304607ba61b386acd31163e092a6504723
Krutarth <krutarthparag.74@hotmail.com>
2017-06-09 20:33:34 +0530

    Adding the Chat Site

---
+++ LoginMain.js
@@ -0,0 +1,32 @@
+var auth = firebase.auth();
+console.log(window.location.pathname);
+
+function Register() {
+
+	var email = document.getElementById('EmailET').value;
+	var password = document.getElementById('PasswordET').value;
+
+	firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+  		// Handle Errors here.
+  		var errorCode = error.code;
+  		var errorMessage = error.message;
+  		console.log(errorCode);
+  		console.log(errorMessage);
+  		// ...
+	});
+	
+}
+
+function Login() {
+
+	var email = document.getElementById('EmailET').value;
+	var password = document.getElementById('PasswordET').value;
+
+	auth.signInWithEmailAndPassword(email, password).catch(function(error){
+
+		console.log(error.code);
+		console.log(error.message);
+
+	});
+	
+}
